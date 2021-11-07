f371100d83390b9775fc1d086ccf65dcd9609d8f
Scratch29 <Jiamingw29@Gmail.com>
2021-11-06 20:20:09 -0400

    task footer now gets tasks from service and changed task selection to buttons

--- src/app/components/task-footer/task-footer.component.ts
+++ src/app/components/task-footer/task-footer.component.ts
@@ -1,5 +1,8 @@
 import {Component, Input, OnInit} from '@angular/core';
 import {Task} from "../../Task";
+import {Subscription} from "rxjs";
+import {TaskServiceService} from "../../services/task-service.service";
+
 
 @Component({
   selector: 'app-task-footer',
@@ -7,12 +10,26 @@ import {Task} from "../../Task";
   styleUrls: ['./task-footer.component.css']
 })
 export class TaskFooterComponent implements OnInit {
-  @Input() tasks!: Task[];
+  tasks!: Task[];
+  subscription!: Subscription;
+  numberOfTasks!: number;
 
-  constructor() {
+  constructor(private taskService: TaskServiceService) {
+
   }
 
   ngOnInit(): void {
+    this.subscription = this.taskService.getTaskList().subscribe((listOfTasks) => {
+      this.tasks = listOfTasks;
+      let taskCounter: number = 0
+      for (let i = 0; i < this.tasks.length; i++) {
+        if (!this.tasks[i].completed) {
+          taskCounter++;
+        }
+      }
+      this.numberOfTasks = taskCounter;
+    })
   }
+
 
 }
